9a27e06d346858eb3c4cbfb4f0926df234ac0e82
candykouty <renka9951@gmail.com>
2016-03-26 18:34:01 +0100

    hearts1

--- public/js/hearts.js
+++ public/js/hearts.js
@@ -3,3 +3,27 @@ var LaTourEiffel = L.marker([48.8582606, 2.2945071000000326], {
 }).addTo(mymap);
 
 // LaTourEiffel.bindPopup('La Tour Eiffel, neodmysliteľný symbol Paríža');
+
+var NotreDame = L.marker([48.85296820000001, 2.3499021000000084], {
+  icon: heartMarker
+}).addTo(mymap);
+
+// NotreDame.bindPopup('Notre Dame, aneb známa katedrála s Quasimodom');
+
+var MuséeDuLouvre = L.marker([48.8606111, 2.3376439999999548], {
+  icon: heartMarker
+}).addTo(mymap);
+
+// MuséeDuLouvre.bindPopup('Musée Du Louvre, skrýva viac ako len Monu Lisu');
+
+var ChampsElysées = L.marker([48.8657844, 2.307314099999985], {
+  icon: heartMarker
+}).addTo(mymap);
+
+// ChampsElysées.bindPopup('Champs Elysées, najslávnejšia ulica vôbec');
+
+var LaDéfense = L.marker([48.891742, 2.240833], {
+  icon: heartMarker
+}).addTo(mymap);
+
+// LaDéfense.bindPopup('La Défense, najmodernejšia urbanistická štvrť');
