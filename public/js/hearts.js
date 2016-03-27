0c08c74eeba2a4c66ac6b129b7d39e7a0e30c19e
candykouty <renka9951@gmail.com>
2016-03-27 12:19:25 +0200

    Hearts Paris

--- public/js/hearts.js
+++ public/js/hearts.js
@@ -27,3 +27,51 @@ var LaDéfense = L.marker([48.891742, 2.240833], {
 }).addTo(mymap);
 
 LaDéfense.bindPopup('La Défense, najmodernejšia urbanistická štvrť');
+
+var ShakespeareCompany = L.marker([48.8525938, 2.347208799999976], {
+  icon: heartMarker
+}).addTo(mymap);
+
+ShakespeareCompany.bindPopup('Shakespeare&Company, najčarovnejšie kníhkupectvo, v akom som kedy bola');
+
+var GibertJeuneLibrarie = L.marker([48.8693196, 2.3542428000000655], {
+  icon: heartMarker
+}).addTo(mymap);
+
+GibertJeuneLibrarie.bindPopup('Gibert Jeune Librarie, ničím výnimočné ale aj tak sympatické kníhkupectvo');
+
+var CaféDesDeuxMoulins = L.marker([48.8849052, 2.3336640999999645], {
+  icon: heartMarker
+}).addTo(mymap);
+
+CaféDesDeuxMoulins.bindPopup('Café des Deux Moulins, kaviarnička slávnej Amélie z Montmartru');
+
+var CinémaStudio28 = L.marker([48.88605519999999, 2.335513100000071], {
+  icon: heartMarker
+}).addTo(mymap);
+
+CinémaStudio28.bindPopup('Cinéma Studio 28, klasické malé kino plné kvalitných filmov');
+
+var LeMurDesJeTAime = L.marker([48.88483319999999, 2.3385485999999673], {
+  icon: heartMarker
+}).addTo(mymap);
+
+LeMurDesJeTAime.bindPopup('Le Mur Des Je T\'Aime, kde sa láska stretáva v každom jazyku');
+
+var Catacombs = L.marker([48.8338325, 2.3324221999999963], {
+  icon: heartMarker
+}).addTo(mymap);
+
+Catacombs.bindPopup('Catacombs, určite si predtým pozrite film \'Pod zemou\'');
+
+var CimètiereDuPèreLachaise = L.marker([48.861393, 2.3933276000000205], {
+  icon: heartMarker
+}).addTo(mymap);
+
+CimètiereDuPèreLachaise.bindPopup('Cimètiere Du Père Lachaise, spája osobnosti ako Oscar Wilde či Edith Piaf');
+
+var CentreGeorgesPompidou = L.marker([48.86064200000001, 2.352245000000039], {
+  icon: heartMarker
+}).addTo(mymap);
+
+CentreGeorgesPompidou.bindPopup('Centre Georges-Pompidou, umelecky zaujímavé ako zvonka tak i zvnútra');
