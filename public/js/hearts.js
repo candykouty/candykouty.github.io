7921d58bfaecdc831326ac67657c336f04c7eec3
candykouty <renka9951@gmail.com>
2016-03-27 20:55:40 +0200

    Hearts Berlin

--- public/js/hearts.js
+++ public/js/hearts.js
@@ -1,77 +1,126 @@
+//Paris
+
 var LaTourEiffel = L.marker([48.8582606, 2.2945071000000326], {
   icon: heartMarker
 }).addTo(mymap);
 
-LaTourEiffel.bindPopup('La Tour Eiffel, neodmysliteľný symbol Paríža');
+LaTourEiffel.bindPopup('La Tour Eiffel; neodmysliteľný symbol Paríža');
 
 var NotreDame = L.marker([48.85296820000001, 2.3499021000000084], {
   icon: heartMarker
 }).addTo(mymap);
 
-NotreDame.bindPopup('Notre Dame, aneb známa katedrála s Quasimodom');
+NotreDame.bindPopup('Notre Dame; aneb známa katedrála s Quasimodom');
 
 var MuséeDuLouvre = L.marker([48.8606111, 2.3376439999999548], {
   icon: heartMarker
 }).addTo(mymap);
 
-MuséeDuLouvre.bindPopup('Musée Du Louvre, skrýva viac ako len Monu Lisu');
+MuséeDuLouvre.bindPopup('Musée Du Louvre; skrýva viac ako len Monu Lisu');
 
 var ChampsElysées = L.marker([48.8657844, 2.307314099999985], {
   icon: heartMarker
 }).addTo(mymap);
 
-ChampsElysées.bindPopup('Champs Elysées, najslávnejšia ulica vôbec');
+ChampsElysées.bindPopup('Champs Elysées; najslávnejšia ulica vôbec');
 
-var LaDéfense = L.marker([48.891742, 2.240833], {
+var LaDéfense = L.marker([48.8712216, 2.3031524000000445], {
   icon: heartMarker
 }).addTo(mymap);
 
-LaDéfense.bindPopup('La Défense, najmodernejšia urbanistická štvrť');
+LaDéfense.bindPopup('La Défense; najmodernejšia urbanistická štvrť');
 
 var ShakespeareCompany = L.marker([48.8525938, 2.347208799999976], {
   icon: heartMarker
 }).addTo(mymap);
 
-ShakespeareCompany.bindPopup('Shakespeare&Company, najčarovnejšie kníhkupectvo, v akom som kedy bola');
+ShakespeareCompany.bindPopup('Shakespeare&Company; najčarovnejšie kníhkupectvo, v akom som kedy bola');
 
 var GibertJeuneLibrarie = L.marker([48.8693196, 2.3542428000000655], {
   icon: heartMarker
 }).addTo(mymap);
 
-GibertJeuneLibrarie.bindPopup('Gibert Jeune Librarie, ničím výnimočné ale aj tak sympatické kníhkupectvo');
+GibertJeuneLibrarie.bindPopup('Gibert Jeune Librarie; ničím výnimočné ale aj tak sympatické kníhkupectvo');
 
 var CaféDesDeuxMoulins = L.marker([48.8849052, 2.3336640999999645], {
   icon: heartMarker
 }).addTo(mymap);
 
-CaféDesDeuxMoulins.bindPopup('Café des Deux Moulins, kaviarnička slávnej Amélie z Montmartru');
+CaféDesDeuxMoulins.bindPopup('Café des Deux Moulins; kaviarnička slávnej Amélie z Montmartru');
 
 var CinémaStudio28 = L.marker([48.88605519999999, 2.335513100000071], {
   icon: heartMarker
 }).addTo(mymap);
 
-CinémaStudio28.bindPopup('Cinéma Studio 28, klasické malé kino plné kvalitných filmov');
+CinémaStudio28.bindPopup('Cinéma Studio 28; klasické malé kino plné kvalitných filmov');
 
 var LeMurDesJeTAime = L.marker([48.88483319999999, 2.3385485999999673], {
   icon: heartMarker
 }).addTo(mymap);
 
-LeMurDesJeTAime.bindPopup('Le Mur Des Je T\'Aime, kde sa láska stretáva v každom jazyku');
+LeMurDesJeTAime.bindPopup('Le Mur Des Je T\'Aime; kde sa láska stretáva v každom jazyku');
 
 var Catacombs = L.marker([48.8338325, 2.3324221999999963], {
   icon: heartMarker
 }).addTo(mymap);
 
-Catacombs.bindPopup('Catacombs, určite si predtým pozrite film \'Pod zemou\'');
+Catacombs.bindPopup('Catacombs; určite si predtým pozrite film \'Pod zemou\'');
 
 var CimètiereDuPèreLachaise = L.marker([48.861393, 2.3933276000000205], {
   icon: heartMarker
 }).addTo(mymap);
 
-CimètiereDuPèreLachaise.bindPopup('Cimètiere Du Père Lachaise, spája osobnosti ako Oscar Wilde či Edith Piaf');
+CimètiereDuPèreLachaise.bindPopup('Cimètiere Du Père Lachaise; spája známe osobnosti ako Oscar Wilde či Edith Piaf');
 
 var CentreGeorgesPompidou = L.marker([48.86064200000001, 2.352245000000039], {
   icon: heartMarker
 }).addTo(mymap);
 
-CentreGeorgesPompidou.bindPopup('Centre Georges-Pompidou, umelecky zaujímavé ako zvonka tak i zvnútra');
+CentreGeorgesPompidou.bindPopup('Centre Georges-Pompidou; umelecky zaujímavé ako zvonka tak i zvnútra');
+
+
+//Berlin
+
+var SpyMuseumBerlin = L.marker([52.5089522, 13.379222099999993], {
+  icon: heartMarker
+}).addTo(mymap);
+
+SpyMuseumBerlin.bindPopup('Spy Museum Berlin; nakuknete do sveta agentov a špionáže');
+
+var CheckpointCharlie = L.marker([52.5075927, 13.390368500000022], {
+  icon: heartMarker
+}).addTo(mymap);
+
+CheckpointCharlie.bindPopup('Checkpoint Charlie; najlepšie investované 3 eurá za fotky s fejkovými vojakmi');
+
+var EastSideGallery = L.marker([52.50502239999999, 13.43969530000004], {
+  icon: heartMarker
+}).addTo(mymap);
+
+EastSideGallery.bindPopup('East Side Gallery; 1,3 km spojeného umenia s históriou');
+
+var BerlinerDom = L.marker([52.5190608, 13.401077999999984], {
+  icon: heartMarker
+}).addTo(mymap);
+
+BerlinerDom.bindPopup('Berliner Dom; Paríž má Notre Dame, Berlín túto majestátnu katedrálu');
+
+var BrandenburgerTor = L.marker([52.5162746, 13.377703999999994], {
+  icon: heartMarker
+}).addTo(mymap);
+
+BrandenburgerTor.bindPopup('Brandenburger Tor, najznámejšia pamiatka Berlína');
+
+var JudischesMuseumBerlin = L.marker([52.5023115, 13.395446900000024], {
+  icon: heartMarker
+}).addTo(mymap);
+
+JudischesMuseumBerlin.bindPopup('Jüdisches Museum Berlin, myšlienkovo silné architektonické dielo');
+
+var Sachsenhausen = L.marker([52.78566619999999, 13.23408459999996], {
+  icon: heartMarker
+}).addTo(mymap);
+
+Sachsenhausen.bindPopup('Sachsenhausen, výcvikové miesto pre veliteľov do koncentračných táborov');
+
+//Praha
